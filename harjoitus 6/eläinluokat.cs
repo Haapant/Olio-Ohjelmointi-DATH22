ca6c51aded6fefd818b535183b635294d5a4cf34
sampo.haapala <sampo.haapala@STUDY22P-532311>
2023-01-10 11:31:07 +0200

    Tehtävä 6

---
+++ harjoitus 6/eläinluokat.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace harjoitus_6
+{
+    internal class Animals
+    {
+        public static int TotalAnimals { get; set; }
+        public virtual string Sound { get; set; }
+        public virtual void MakeSound()
+        {
+            Console.WriteLine("This animal makes a generic sound.");
+        }
+        public Animals()
+        {
+            TotalAnimals++;
+        }
+    }
+
+    class Dog : Animals
+    {
+        public override string Sound { get; set; } = "Woof";
+        public override void MakeSound()
+        {
+            Console.WriteLine("The dog says " + Sound);
+        }
+    }
+    class Cat : Animals
+    {
+        public override string Sound { get; set; } = "Meow";
+        public override void MakeSound()
+        {
+            Console.WriteLine("The cat says " + Sound);
+        }
+    
+
